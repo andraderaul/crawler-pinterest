3066512786963e70570b3a58d7ab987bfbd80c87
Raul Andrade <theandraderaul@gmail.com>
2020-11-20 23:17:53 -0300

    improvement in crawler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const fetch = require("node-fetch");
 
+const IMAGE_DIRECTORY = "images";
+
 async function download(uri, filename, callback) {
   const res = await fetch(uri);
   await new Promise((resolve, reject) => {
@@ -17,26 +19,63 @@ async function download(uri, filename, callback) {
   });
 }
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: true });
+function createDirectory(name) {
+  const dir = `${IMAGE_DIRECTORY}/${name}`;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+}
 
-  const page = await browser.newPage();
-  await page.goto("https://br.pinterest.com/search/pins/?q=bom%20dia", {
+function getExtension(uri) {
+  const parts = uri.split(".");
+  return parts[parts.length - 1];
+}
+
+async function downloadImages(uriImgs, imageName) {
+  console.log(uriImgs.length);
+
+  createDirectory(imageName);
+  uriImgs.forEach(async (uri, index) => {
+    const extension = getExtension(uri);
+    const path = `${IMAGE_DIRECTORY}/${imageName}/${imageName}${index}.${extension}`;
+
+    await download(uri, path, function () {
+      console.log("done");
+    });
+  });
+}
+
+async function getImages(page, uri) {
+  await page.goto(uri, {
     waitUntil: "networkidle0",
   });
 
   await page.screenshot({ path: "example.png" });
 
-  const srcImgs = await page.$$eval("img", (imgs) =>
-    imgs.map((img) => img.srcset)
+  const uriImgs = await page.$$eval("img", (imgs) =>
+    imgs
+      .filter((img) => img.srcset !== "")
+      .map((img) => img.srcset.split(", ")[1].split(" ")[0])
   );
 
-  srcImgs.forEach((srcImg, index) => {
-    const src = srcImg.split(", ")[1].split(" ")[0];
-    download(src, `images/img${index}.png`, function () {
-      console.log("done");
-    });
-  });
+  return uriImgs;
+}
 
-  await browser.close();
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  try {
+    const page = await browser.newPage();
+    page.setViewport({ width: 1280, height: 926 });
+
+    const uriImgs = await getImages(
+      page,
+      "https://br.pinterest.com/search/pins/?q=bom%20dia%20whatsapp"
+    );
+
+    await downloadImages(uriImgs, "bom_dia");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await browser.close();
+  }
 })();
